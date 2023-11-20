6e705df98e2237e5f0939bc40fd5d97cce6848cd
PatrickRanval <pat.ranval@gmail.com>
2023-11-20 16:42:42 -0600

    More Forms
    Significant Expansion of Information Displayed

--- src/app/seed-shelf/seed-on-shelf/edit-seed-on-shelf/edit-seed-on-shelf.component.ts
+++ src/app/seed-shelf/seed-on-shelf/edit-seed-on-shelf/edit-seed-on-shelf.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Seed } from 'src/app/shared/models/seed.model';
+import { Story } from 'src/app/shared/models/story.model';
 import { SeedService } from 'src/app/shared/services/seed.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
@@ -17,11 +18,13 @@ export class EditSeedOnShelfComponent implements OnInit, OnDestroy {
   seedSubscription: Subscription;
   editForm: FormGroup;
   myParamMap: Subscription;
+  additionalTaxonomy = false;
+  addStory = false;
 
   constructor(
     private route: ActivatedRoute,
     private seedService: SeedService,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -37,9 +40,6 @@ export class EditSeedOnShelfComponent implements OnInit, OnDestroy {
     this.seedService.setSelectedSeedById(this.id);
   }
 
-  ngOnDestroy() {
-    this.seedSubscription.unsubscribe();
-  }
 
   buildForm() {
     if (this.specificSeed) {
@@ -47,28 +47,59 @@ export class EditSeedOnShelfComponent implements OnInit, OnDestroy {
         'genus': new FormControl(this.specificSeed.genus),
         'species': new FormControl(this.specificSeed.species),
         'variety': new FormControl(this.specificSeed.variety),
-        'commonBroad': new FormControl(this.specificSeed.commonBroad.join(', ')), // Assuming commonBroad is an array
+        'commonBroad': new FormControl(this.specificSeed.commonBroad.join(', ')),
         'imgURL': new FormControl(this.specificSeed.imgURL),
+        'subspecies': new FormControl(this.specificSeed.subspecies),
+        'hybridStatus': new FormControl(this.specificSeed.hybridStatus),
+        'subvariety': new FormControl(this.specificSeed.subvariety),
+        'commonNarrow': new FormControl(
+          this.specificSeed.commonNarrow ? this.specificSeed.commonNarrow.join(', ') : null
+        ),
+        'instance': new FormControl(this.specificSeed.instance),
+        'story': new FormControl(this.specificSeed.story)
       });
     }
   }
 
-  onSubmit() {
-    if (this.editForm.valid) {
-      let editedSeed: Seed = new Seed(
-        this.specificSeed.uid,
-        this.editForm.value.genus,
-        this.editForm.value.species,
-        this.editForm.value.variety,
-        this.editForm.value.commonBroad.split(', '), // Convert back to an array
-        this.editForm.value.imgURL
-      );
-      this.seedService.editSeedOnShelf(editedSeed, this.id);
-      this.seedService.setSelectedSeedById(this.id); // emits the updated seed
-      this.router.navigate(['/shelf', this.id]);
-    } else {
-      // Handle form validation errors
-      return;
-    }
+  toggleAdditionalTaxonomy() {
+    this.additionalTaxonomy = !this.additionalTaxonomy;
+  }
+
+  toggleStory() {
+    this.addStory = !this.addStory;
+  }
+
+onSubmit() {
+  if (this.editForm.valid) {
+    let newStory:Story = new Story(
+      [this.editForm.value.storyTags],
+      this.editForm.value.storyText
+    )
+    let editedSeed:Seed = new Seed(
+      this.specificSeed.uid,
+      this.editForm.value.genus,
+      this.editForm.value.species,
+      this.editForm.value.variety,
+      this.editForm.value.commonBroad.split(', '),
+      this.editForm.value.imgURL,
+      this.editForm.value.subspecies || null,
+      this.editForm.value.hybridStatus || null,
+      this.editForm.value.subvariety || null,
+      this.editForm.value.commonNarrow ? this.editForm.value.commonNarrow.split(', ') : null,
+      this.editForm.value.instance || null,
+      [newStory] || null );
+
+    // Your existing logic for editing and navigation
+    this.seedService.editSeedOnShelf(editedSeed, this.id);
+    this.seedService.setSelectedSeedById(this.id); // emits the updated seed
+    this.router.navigate(['/shelf', this.id]);
+  } else {
+    // Handle form validation errors
+    return;
+  }
+}
+
+  ngOnDestroy() {
+    this.seedSubscription.unsubscribe();
   }
 }
